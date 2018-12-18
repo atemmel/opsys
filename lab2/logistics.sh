cd80c7cab62ef40442647c9cf75e4a6d915abf72
atemmel <temmeladam@gmail.com>
2018-12-18 09:37:01 +0100

    Skalat av

--- lab2/logistics.sh
+++ lab2/logistics.sh
@@ -6,7 +6,15 @@ SEP=','
 TABLE='BEGIN{printf("%3s\t%12s\t%5s\t%3s\t%3s\t%3s\n","ID","Name","Weight","L","B","H")}{printf("%3s\t%12s\t%5d\t%3d\t%3d\t%3d\n",$1,$2,$3,$4,$5,$6)}'
 
 usage() { 
-	echo "Här är hjälptext." 
+	echo "Usage: logistics  FILE [-b|-p|-s {i|n|v|l|b|h}]
+Used  for  logistics  management  with  FILE as  underlying
+data.
+-b       generate  backup  copy of data  contents
+-p       print  data  contents  and  exit
+-s       sort by  additional  argument: id (i),
+name (n), weight (v), length (l)
+width (b), height (h), print  data
+contents  and  exit" 
 }
 
 check_file() {
@@ -40,13 +48,76 @@ sort_table() {
 	esac
 
 	if [ -z "$ARGS" ] ; then
-		echo "Ajsing bajsing"
+		echo "Column to sort on not recognized"
 		exit 1
 	fi
 	
 	#echo $ARGS $FILE | awk -F$SEP $TABLE
 	sort $ARGS $FILE | awk -F$SEP $TABLE
 }
+
+interactive_help(){
+	echo "  HELP"
+	echo "f : Select file to read from"
+	echo "p : Print file contents"
+	echo "s : Sort file contents"
+	echo "b : Backup file"
+	echo "q : Quit program"
+	echo "h : Show help"
+}
+
+filenotset(){
+	echo "Error, file is not set. Set file with 'f'"
+}
+
+if [ -z "$1" ] ; then
+	interactive_help;
+	while true ; do
+		read input;
+
+		case $input in
+			b) 
+				if [ ! -f $FILE ] ; then
+					filenotset 
+					continue
+				fi 
+				backup_table;
+				echo "Backup generated"
+			;;
+			p) 
+				if [ ! -f $FILE ] ; then
+					filenotset 
+					continue
+				fi 
+				print_table
+			;;
+			s)  
+				if [ ! -f $FILE ] ; then
+					filenotset 
+					continue
+				fi 
+				echo "Name of column to sort on:"
+				echo "i : id, n : name, v : weight, l : length, b : width, h : height"
+				read COLUMN;
+				sort_table
+			;;
+			f) 
+				echo "Name of file to read:"
+				read input;
+				FILE="$input"
+				if [ ! -f $FILE ] ; then
+					echo "File $FILE not found"
+				else
+					echo "File set"
+				fi
+			;;
+			h) interactive_help;;
+			q) exit 0;;
+			?) echo "Unrecognized command, type 'h' for help"
+		esac
+	done
+fi
+
 
 while getopts "hbps:f:" o; do
 
