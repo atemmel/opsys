c4d80d9a53b4633a6b447b394d2432431c8b0c06
atemmel <temmeladam@gmail.com>
2018-12-18 05:33:49 +0100

    Jag vill dö

--- lab2/logistics.bat
+++ lab2/logistics.bat
@@ -1,4 +1,7 @@
 @echo off
+
+rem Förlåt mig Jimpan, ty jag har syndat
+
 setlocal EnableDelayedExpansion
 ::chcp 65001>nul
 chcp 1252>nul
@@ -14,6 +17,162 @@ set col_w_4=3
 set col_w_5=3
 set length=
 set col_to_read=
+set header=ID  NAME         VIKT  L   B   H
+
+:READ_ARGS
+	if "%1" == "" (
+		call :PRINT_HELP_INTERACTIVE
+		goto :INTERACTIVE
+	)
+	if "%1" == "/?" (
+		call :PRINT_HELP
+		goto :EXIT
+	)
+
+	if "%2" == "" ( goto :BAD)
+
+	set file=%1
+	call :OPEN_FILE_MATRIX
+
+	if /i "%2" == "/print" (
+		call :PRINT_MAT
+		goto :EXIT
+	)
+
+	if "%3" == "" ( goto :BAD)
+	if /i "%2" == "/sort" (
+		set col_to_read=
+
+		if /i "%3" == "i" ( set col_to_read=0 )
+		if /i "%3" == "n" ( set col_to_read=1 )
+		if /i "%3" == "v" ( set col_to_read=2 )
+		if /i "%3" == "l" ( set col_to_read=3 )
+		if /i "%3" == "b" ( set col_to_read=4 )
+		if /i "%3" == "h" ( set col_to_read=5 )
+		if "!col_to_read!"=="" ( goto :BAD )
+		
+		call :OPEN_FILE
+		call :BUBBLE
+		call :PRINT_MAT
+		
+		goto :EXIT
+	)
+	if /i "%2" == "/backup" (
+		set  backup_file=%3
+		call :BACKUP
+	)
+
+	goto :EXIT
+)
+
+:INTERACTIVE
+	choice /c FPSBQH
+
+	if ERRORLEVEL 6 (
+		call :PRINT_HELP_INTERACTIVE
+		goto :INTERACTIVE
+	)
+
+	if ERRORLEVEL 5 goto :EXIT
+
+	rem BACKUP
+	if ERRORLEVEL 4 (
+		if "!file!" == "" (
+			echo.
+			echo FILE HAS NOT BEEN SPECIFIED
+			echo.
+			goto :INTERACTIVE
+		)
+		echo.
+		set /p backup_file="Enter name of backup file: "
+		echo.
+		call :BACKUP
+		echo.
+		echo !file! copied to !backup_file!
+		echo.
+		goto :INTERACTIVE
+	)
+
+	rem SORT_FILE
+	if ERRORLEVEL 3 (
+		if "!file!" == "" (
+			echo.
+			echo FILE HAS NOT BEEN SPECIFIED
+			echo.
+			goto :INTERACTIVE
+		)
+		:READ_COLUMN_FAIL
+		set col_to_read=
+		echo.
+		echo Sort by:
+		echo i = id [] n = name [] v = weight [] l = length [] b = width [] h = height
+		echo.
+		set /p input="Enter column to sort by: "
+		if /i "!input!" == "i" ( set col_to_read=0 )
+		if /i "!input!" == "n" ( set col_to_read=1 )
+		if /i "!input!" == "v" ( set col_to_read=2 )
+		if /i "!input!" == "l" ( set col_to_read=3 )
+		if /i "!input!" == "b" ( set col_to_read=4 )
+		if /i "!input!" == "h" ( set col_to_read=5 )
+		if "!col_to_read!"=="" ( 
+			echo.
+			echo ERROR COLUMN !input! DOES NOT EXIST
+			echo.
+			goto :READ_COLUMN_FAIL 
+		)
+		call :OPEN_FILE
+		call :BUBBLE
+		echo.
+		echo SORTED
+		echo.
+		goto :INTERACTIVE
+	)
+	
+	rem PRINT_FILE
+	if ERRORLEVEL 2 (
+		if "!file!" == "" (
+			echo.
+			echo FILE HAS NOT BEEN SPECIFIED
+			echo.
+			goto :INTERACTIVE
+		)
+		echo.
+		call :PRINT_MAT
+		echo.
+		goto :INTERACTIVE
+	)
+	
+	rem OPEN_FILE
+	if ERRORLEVEL 1 (
+		:READ_FILE_FAIL
+		echo.
+		set /p file="Enter path to file: " 
+		echo.
+		if exist !file! (
+			call :OPEN_FILE_MATRIX
+			goto :INTERACTIVE
+		)
+		echo.
+		echo ERROR: FILE !file! DOES NOT EXIST
+		echo.
+		goto :READ_FAIL
+	)
+	
+:INTERACTIVE_END
+	
+
+:READ_ARGS_END
+
+:BAD
+	echo MISSING ARGUMENT, SPECIFY /? FOR HELP
+	goto EXIT
+:BAD_END
+
+:BACKUP
+	if %backup_file% == "" goto :EOF
+	COPY "%file%" "%backup_file%" >nul
+	goto :EOF
+:BACKUP_END
 
 :OPEN_FILE_MATRIX
 	set /a width-=1
@@ -26,16 +185,6 @@ set col_to_read=
 			set mat_%%a_%%b=!arr_%%b!
 		)
 	)
-
-	set col_to_read=1
-	call :OPEN_FILE
-	
-	call :BUBBLE
-	
-	call :PRINT_MAT
-	
-	::call :PRINT_ARR
-
 	goto :EOF
 :OPEN_FILE_MATRIX_END
 
@@ -92,6 +241,7 @@ goto EXIT
 :PRINT_ARR_END
 
 :PRINT_MAT
+	echo %header%
 	for /l %%y in (0,1,%height%) do (
 		set output_str=
 		set tmp_str=
@@ -115,6 +265,34 @@ goto EXIT
 		set tmp_str=!space_str!
 		set space_str= !tmp_str!
 	)
+	goto :EOF
 :MAKE_N_SPACE_END
+
+:PRINT_HELP
+	echo Används för logistikhantering
+	echo.
+	echo Syntax logistics [enhet:]sökväg /backup, /print , /sort i n v l b h
+	echo.
+	echo /backup	Genererar backupfil enligt argument
+	echo /print		Skriver ut innehåll i filen
+	echo /sort		Sorterar och skriver ut innehåll i filen
+	echo i = id [] n = name [] v = weight [] l = length [] b = width [] h = height
+	echo.
+
+	goto :EOF
+:PRINT_HELP_END
+
+:PRINT_HELP_INTERACTIVE
+	echo COMMANDS:
+	echo.
+	echo F Select file
+	echo P Print file contents
+	echo S Sort file output
+	echo B Create backup
+	echo Q Quit
+	echo H Prints help
+	echo.
+	goto :EOF
+:PRINT_HELP_INTERACTIVE_END
 
 :EXIT
